061cfd528df94083c15512bc102c81445d20d825
salvamuletmorales <116027990+salvamuletmorales@users.noreply.github.com>
2023-03-13 20:04:32 +0100

    Agregar archivos de proyecto.

---
+++ Coche.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParqueVehiculos
+{
+    internal class Coche : Vehiculo
+    {
+        private int numAirbags;
+        private bool techoSolar;
+
+        public Coche():base()
+        {
+        }
+
+
+        public Coche(int numAirbags, bool techoSolar): base()
+        {
+            this.numAirbags = numAirbags;
+            this.techoSolar = techoSolar;
+        }
+
+        public override void Acelerar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Arrancar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Frenar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Vehiculo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParqueVehiculos
+{
+    internal abstract class  Vehiculo
+    {
+        protected string Matricula { get; set; }
+        protected string Marca { get; set; }
+        protected string Modelo { get; set; }
+        protected string Color { get; set; }
+        protected int Km { get; set; }
+        protected int NumPlazas { get; set; }
+        protected int NumPuertas { get; set; }
+
+        public static int NUM_VEHICULOS = 0;
+
+        public const int MAX_VEHICULOS = 5;
+
+        protected Vehiculo()
+        {
+            Matricula = matAleatorio();
+            Color = "blanco";
+            Km = 0;
+        }
+
+        protected Vehiculo( string marca, string modelo, string color, int km, int numPlazas, int numPuertas):this()
+        {
+            
+            Marca = marca;
+            Modelo = modelo;
+            Color = color;
+            Km = km;
+            NumPlazas = numPlazas;
+            NumPuertas = numPuertas;
+        }
+
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+        
+       
+
+        public static String matAleatorio()
+        {
+            Random rand = new Random();
+            
+            String matricula = rand.Next(1000,9999).ToString();
+
+            
+            return matricula;
+        }
+
+        public abstract void Arrancar();
+        public abstract void Acelerar();
+        public abstract void Frenar();
+
+
+    }
+}
